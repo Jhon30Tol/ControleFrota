3692f49f87cb2e916242c51a5d9c308c1465b8d3
Jonathas Toledo <48101425+Jhon30Tol@users.noreply.github.com>
2019-05-12 09:05:52 -0300

    Atualização Mensagens do Sistema

--- PimFrota/Formularios/Mensagens/FrmMensagemCampoObrigatorio.Designer.cs
+++ PimFrota/Formularios/Mensagens/FrmMensagemCampoObrigatorio.Designer.cs
@@ -30,9 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMensagemCampoObrigatorio));
             this.MensagemOkBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.MsgFecharPnl = new System.Windows.Forms.Panel();
             this.Fecharbtn = new System.Windows.Forms.Button();
-            this.MsgCampoObrigatorioLbl = new MaterialSkin.Controls.MaterialLabel();
+            this.MensageCampoObrigatorioLbl = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.MsgFecharPnl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,12 +44,22 @@
             this.MensagemOkBtn.FlatAppearance.BorderSize = 0;
             this.MensagemOkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.MensagemOkBtn.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MensagemOkBtn.Location = new System.Drawing.Point(111, 81);
+            this.MensagemOkBtn.Location = new System.Drawing.Point(115, 82);
             this.MensagemOkBtn.Name = "MensagemOkBtn";
             this.MensagemOkBtn.Size = new System.Drawing.Size(75, 23);
             this.MensagemOkBtn.TabIndex = 28;
             this.MensagemOkBtn.Text = "OK";
             this.MensagemOkBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.MensageCampoObrigatorioLbl);
+            this.panel1.Controls.Add(this.MsgFecharPnl);
+            this.panel1.Controls.Add(this.MensagemOkBtn);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 117);
+            this.panel1.TabIndex = 29;
             // 
             // MsgFecharPnl
             // 
@@ -56,8 +68,8 @@
             this.MsgFecharPnl.Dock = System.Windows.Forms.DockStyle.Top;
             this.MsgFecharPnl.Location = new System.Drawing.Point(0, 0);
             this.MsgFecharPnl.Name = "MsgFecharPnl";
-            this.MsgFecharPnl.Size = new System.Drawing.Size(298, 20);
-            this.MsgFecharPnl.TabIndex = 27;
+            this.MsgFecharPnl.Size = new System.Drawing.Size(320, 20);
+            this.MsgFecharPnl.TabIndex = 29;
             // 
             // Fecharbtn
             // 
@@ -70,44 +82,41 @@
             this.Fecharbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Fecharbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.Fecharbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Fecharbtn.Location = new System.Drawing.Point(276, 3);
+            this.Fecharbtn.Location = new System.Drawing.Point(298, 3);
             this.Fecharbtn.Name = "Fecharbtn";
             this.Fecharbtn.Size = new System.Drawing.Size(19, 15);
             this.Fecharbtn.TabIndex = 0;
             this.Fecharbtn.UseVisualStyleBackColor = false;
             // 
-            // MsgCampoObrigatorioLbl
+            // MensageCampoObrigatorioLbl
             // 
-            this.MsgCampoObrigatorioLbl.AutoSize = true;
-            this.MsgCampoObrigatorioLbl.Depth = 0;
-            this.MsgCampoObrigatorioLbl.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MsgCampoObrigatorioLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.MsgCampoObrigatorioLbl.Location = new System.Drawing.Point(8, 47);
-            this.MsgCampoObrigatorioLbl.MouseState = MaterialSkin.MouseState.HOVER;
-            this.MsgCampoObrigatorioLbl.Name = "MsgCampoObrigatorioLbl";
-            this.MsgCampoObrigatorioLbl.Size = new System.Drawing.Size(287, 18);
-            this.MsgCampoObrigatorioLbl.TabIndex = 26;
-            this.MsgCampoObrigatorioLbl.Text = "Preencha os campos Obrigatórios.";
+            this.MensageCampoObrigatorioLbl.AutoSize = true;
+            this.MensageCampoObrigatorioLbl.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MensageCampoObrigatorioLbl.Location = new System.Drawing.Point(30, 50);
+            this.MensageCampoObrigatorioLbl.Name = "MensageCampoObrigatorioLbl";
+            this.MensageCampoObrigatorioLbl.Size = new System.Drawing.Size(257, 16);
+            this.MensageCampoObrigatorioLbl.TabIndex = 30;
+            this.MensageCampoObrigatorioLbl.Text = "Preencha os campos Obrigatórios.";
             // 
             // FrmMensagemCampoObrigatorio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(298, 116);
-            this.Controls.Add(this.MensagemOkBtn);
-            this.Controls.Add(this.MsgFecharPnl);
-            this.Controls.Add(this.MsgCampoObrigatorioLbl);
+            this.ClientSize = new System.Drawing.Size(320, 117);
+            this.Controls.Add(this.panel1);
             this.Name = "FrmMensagemCampoObrigatorio";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.MsgFecharPnl.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button MensagemOkBtn;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel MsgFecharPnl;
         private System.Windows.Forms.Button Fecharbtn;
-        private MaterialSkin.Controls.MaterialLabel MsgCampoObrigatorioLbl;
+        private System.Windows.Forms.Label MensageCampoObrigatorioLbl;
     }
 }
--- PimFrota/Formularios/Mensagens/FrmMensagemErroCadastro.Designer.cs
+++ PimFrota/Formularios/Mensagens/FrmMensagemErroCadastro.Designer.cs
@@ -29,13 +29,37 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMensagemErroCadastro));
+            this.ErroPctbx = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.MsgFecharPnl = new System.Windows.Forms.Panel();
             this.Fecharbtn = new System.Windows.Forms.Button();
-            this.MsgErroLbl = new MaterialSkin.Controls.MaterialLabel();
-            this.ErroPctbx = new System.Windows.Forms.PictureBox();
-            this.MsgFecharPnl.SuspendLayout();
+            this.MensagemErroLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ErroPctbx)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.MsgFecharPnl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ErroPctbx
+            // 
+            this.ErroPctbx.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ErroPctbx.BackgroundImage")));
+            this.ErroPctbx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ErroPctbx.Location = new System.Drawing.Point(119, 77);
+            this.ErroPctbx.Name = "ErroPctbx";
+            this.ErroPctbx.Size = new System.Drawing.Size(63, 27);
+            this.ErroPctbx.TabIndex = 31;
+            this.ErroPctbx.TabStop = false;
+            this.ErroPctbx.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.MensagemErroLbl);
+            this.panel1.Controls.Add(this.MsgFecharPnl);
+            this.panel1.Controls.Add(this.ErroPctbx);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 117);
+            this.panel1.TabIndex = 32;
             // 
             // MsgFecharPnl
             // 
@@ -44,8 +68,8 @@
             this.MsgFecharPnl.Dock = System.Windows.Forms.DockStyle.Top;
             this.MsgFecharPnl.Location = new System.Drawing.Point(0, 0);
             this.MsgFecharPnl.Name = "MsgFecharPnl";
-            this.MsgFecharPnl.Size = new System.Drawing.Size(298, 20);
-            this.MsgFecharPnl.TabIndex = 30;
+            this.MsgFecharPnl.Size = new System.Drawing.Size(318, 20);
+            this.MsgFecharPnl.TabIndex = 32;
             // 
             // Fecharbtn
             // 
@@ -58,56 +82,41 @@
             this.Fecharbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Fecharbtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.Fecharbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Fecharbtn.Location = new System.Drawing.Point(276, 3);
+            this.Fecharbtn.Location = new System.Drawing.Point(296, 3);
             this.Fecharbtn.Name = "Fecharbtn";
             this.Fecharbtn.Size = new System.Drawing.Size(19, 15);
             this.Fecharbtn.TabIndex = 0;
             this.Fecharbtn.UseVisualStyleBackColor = false;
-            this.Fecharbtn.Click += new System.EventHandler(this.Fecharbtn_Click);
             // 
-            // MsgErroLbl
+            // MensagemErroLbl
             // 
-            this.MsgErroLbl.AutoSize = true;
-            this.MsgErroLbl.Depth = 0;
-            this.MsgErroLbl.Font = new System.Drawing.Font("Roboto", 11F);
-            this.MsgErroLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.MsgErroLbl.Location = new System.Drawing.Point(43, 37);
-            this.MsgErroLbl.MouseState = MaterialSkin.MouseState.HOVER;
-            this.MsgErroLbl.Name = "MsgErroLbl";
-            this.MsgErroLbl.Size = new System.Drawing.Size(166, 19);
-            this.MsgErroLbl.TabIndex = 29;
-            this.MsgErroLbl.Text = "Cadastro não efetuado.";
-            // 
-            // ErroPctbx
-            // 
-            this.ErroPctbx.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ErroPctbx.BackgroundImage")));
-            this.ErroPctbx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ErroPctbx.Location = new System.Drawing.Point(113, 70);
-            this.ErroPctbx.Name = "ErroPctbx";
-            this.ErroPctbx.Size = new System.Drawing.Size(79, 34);
-            this.ErroPctbx.TabIndex = 31;
-            this.ErroPctbx.TabStop = false;
-            this.ErroPctbx.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.MensagemErroLbl.AutoSize = true;
+            this.MensagemErroLbl.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MensagemErroLbl.Location = new System.Drawing.Point(63, 48);
+            this.MensagemErroLbl.Name = "MensagemErroLbl";
+            this.MensagemErroLbl.Size = new System.Drawing.Size(183, 16);
+            this.MensagemErroLbl.TabIndex = 33;
+            this.MensagemErroLbl.Text = "Operação não efetuada.";
             // 
             // FrmMensagemErroCadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(298, 116);
-            this.Controls.Add(this.ErroPctbx);
-            this.Controls.Add(this.MsgFecharPnl);
-            this.Controls.Add(this.MsgErroLbl);
+            this.ClientSize = new System.Drawing.Size(320, 117);
+            this.Controls.Add(this.panel1);
             this.Name = "FrmMensagemErroCadastro";
-            this.MsgFecharPnl.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ErroPctbx)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.MsgFecharPnl.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox ErroPctbx;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label MensagemErroLbl;
         private System.Windows.Forms.Panel MsgFecharPnl;
         private System.Windows.Forms.Button Fecharbtn;
-        private MaterialSkin.Controls.MaterialLabel MsgErroLbl;
-        private System.Windows.Forms.PictureBox ErroPctbx;
     }
 }
