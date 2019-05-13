73274ef9eac7f957b9b2270365b4159a242c7ea4
Jonathas Toledo <48101425+Jhon30Tol@users.noreply.github.com>
2019-05-13 12:51:25 -0300

    Exclusão de Interfaces temporárias

---
+++ PimFrota/Relatorios/RptrRelatorio.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace PimFrota.Relatorios
+{
+    partial class RptrRelatorio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RptrRelatorio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(597, 395);
+            this.Name = "RptrRelatorio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
