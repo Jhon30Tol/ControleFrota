926e30416a3c8d9e77cc2c98c0c4ac4ee3d00f20
Paluan Batista <paluan-dev666@users.noreply.github.com>
2019-05-13 21:33:37 -0300

    criacao libBuscaCep
    criacao libBuscaCep

---
+++ BuscaCep/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace BuscaCep
+    {
+    public class Class1
+        {
+        /*private void btnMapIt_Click(object sender, EventArgs e)
+
+            {
+
+            try
+
+                {
+
+                string street = string.Empty;
+
+                string city = string.Empty;
+
+                string state = string.Empty;
+
+                string zip = string.Empty;
+
+
+
+                StringBuilder queryAddress = new StringBuilder();
+
+                queryAddress.Append("http://maps.google.com/maps?q=");
+
+
+
+                if (.Text != string.Empty)
+
+                    {
+
+                    street = txtStreet.Text.Replace(' ', '+');
+
+                    queryAddress.Append(street + ',' + '+');
+
+                    }
+
+
+
+                if (txtCity.Text != string.Empty)
+
+                    {
+
+                    city = txtCity.Text.Replace(' ', '+');
+
+                    queryAddress.Append(city + ',' + '+');
+
+                    }
+
+
+
+                if (txtState.Text != string.Empty)
+
+                    {
+
+                    state = txtState.Text.Replace(' ', '+');
+
+                    queryAddress.Append(state + ',' + '+');
+
+                    }
+
+
+
+                if (txtZipCode.Text != string.Empty)
+
+                    {
+
+                    zip = txtZipCode.Text.ToString();
+
+                    queryAddress.Append(zip);
+
+                    }
+
+
+
+                webBrowser1.Navigate(queryAddress.ToString());
+
+                }
+
+            catch (Exception ex)
+
+                {
+
+                MessageBox.Show(ex.Message.ToString(), "Unable to Retrieve Map");
+
+                }
+
+            }*/
+        }
+    }
